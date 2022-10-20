42bbe111754fcbc74835c98417430ad8e0964e11
Julia Savinchenko <Savinchenko0@mail.ru>
2022-10-21 01:44:17 +0700

    lab 5

---
+++ index.js
@@ -0,0 +1,80 @@
+const popUp = document.getElementById('pop_up');
+const popUp2 = document.getElementById('pop_up2');
+const popUp3 = document.getElementById('pop_up3');
+
+const openPopUp1 = document.getElementById('open_pop_up1');
+const closePopUp1 = document.getElementById('pop_up_close1');
+
+const openPopUp2 = document.getElementById('open_pop_up2');
+const closePopUp2 = document.getElementById('pop_up_close2');
+
+const openPopUp3 = document.getElementById('open_pop_up3');
+const closePopUp3 = document.getElementById('pop_up_close3');
+
+openPopUp1.addEventListener('click', function(e){
+    e.preventDefault();
+    popUp.classList.add('active');
+})
+
+closePopUp1.addEventListener('click', function(){
+    popUp.classList.remove('active');
+})
+
+openPopUp2.addEventListener('click', function(e){
+    e.preventDefault();
+    popUp2.classList.add('active');
+})
+
+closePopUp2.addEventListener('click', function(){
+    popUp2.classList.remove('active');
+})
+
+openPopUp3.addEventListener('click', function(e){
+    e.preventDefault();
+    popUp3.classList.add('active');
+})
+
+closePopUp3.addEventListener('click', function(){
+    popUp3.classList.remove('active');
+})
+
+
+// let btnOne = document.querySelector("#open_pop_up1");
+// let btnTwo = document.querySelector("#open_pop_up2");
+// let btnThree = document.querySelector("#open_pop_up3");
+// let popup = document.querySelectorAll(".pop_up_container");
+
+// function togglePopup(index){
+//     popup[index].classList.toggle("popup_opened");
+//   }
+
+//   btnOne.addEventListener( 'click', ()=> togglePopup(0) );
+//   btnTwo.addEventListener( 'click', ()=> togglePopup(1) );
+//   btnThree.addEventListener( 'click', ()=> togglePopup(2) );
+
+
+block1.onmouseover = function (){
+    block1.style.background = "#d34949";
+};
+
+block1.onmouseleave = function () {
+    block1.style.background = "aquamarine";
+};
+
+block2.onmouseover = function (){
+    block2.style.background = "#d34949";
+};
+
+block2.onmouseleave = function () {
+    block2.style.background = "aquamarine";
+};
+
+block3.onmouseover = function (){
+    block3.style.background = "#d34949";
+};
+
+block3.onmouseleave = function () {
+    block3.style.background = "aquamarine";
+};
+
+
